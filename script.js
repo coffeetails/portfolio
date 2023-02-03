0dcc92fb45367086bc5ac9af27e51f962ec2d948
Monica Björk <m.bjork95@gmail.com>
2023-02-03 21:58:35 +0100

    added json file with projects

--- script.js
+++ script.js
@@ -29,3 +29,27 @@ function setTheme(themeName) {
     document.body.style.backgroundImage = "url(\"background-light.svg\")";
  }
 })();
+
+
+
+fetch("projects.json")
+    .then(response => response.json())
+    .then(data => { 
+        console.log(data);
+        data.map(item => {
+            const project = `
+                <section class="projectBox clayit">
+                    <figure class="clayPic">
+                        <img src="${item.image}" />
+                    </figure>
+                    <h2>${item.title}</h2>
+                    <p>${item.text}</p>
+                    <ul>
+                        <li><a href="${item.livePage}">Se sidan live</a></li>
+                        <li><a href="${item.githubPage}">Github repo</a></li>
+                    </ul>
+                </section>`;
+            
+            document.querySelector(".wrapper").innerHTML += project;
+        });
+    });
