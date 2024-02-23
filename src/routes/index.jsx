7c23c99c78e7d1414af344170144951195c3afe7
Monica Björk <m.bjork95@gmail.com>
2024-02-23 12:48:44 +0100

    added placeholder text

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 export default function Index() {
 
     return (
-      <p>Startpage</p>
+      <p>Att utnyttja min erfarenhet som industrielektriker och lagerarbetare ger mig en praktisk erfarenhet som jag kan använda för att skapa webbapplikationer som inte bara uppfyller tekniska krav utan också fokuserar på en förbättrad användarvänlighet och design.</p>
     );
   }
