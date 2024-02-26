ff890286459aae09147d9133e15d9acdc6f3dc16
Monica Björk <m.bjork95@gmail.com>
2024-02-26 21:44:04 +0100

    figuring out SVGs. Still.

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,22 +1,32 @@
 import { Outlet, Link } from "react-router-dom";
 // <Outlet /> is where the content will show
+import { useLayoutEffect, useState, useRef } from "react";
 
 import './root.css';
 import WaveOne from '/wave1.svg';
 import WaveTwo from '/wave2.svg';
 import WaveThree from '/wave3.svg';
+import WaveTest from './waveTest';
 
 export default function Root() {
-    console.log(WaveThree);
+    let headerElem = useRef(null);
+    let [height, setHeight] = useState(null);
+    let [width, setWidth] = useState(null);
+    useLayoutEffect(() => setHeight(headerElem.current.offsetHeight));
+    useLayoutEffect(() => setWidth(headerElem.current.offsetWidth));
+    useLayoutEffect(() => console.log(headerElem));
 
     return (
         <>
-        <header>
-            <h1>Header</h1>
+        <header ref={headerElem}>
+            <h1 className="header-text">{width + " x " + height}</h1>
             {/* <div className="wrapper-wave"> */}
-                <object data={WaveOne} type="image/svg+xml"></object>
-                <object data={WaveTwo} type="image/svg+xml"></object>
-                <object data={WaveThree} type="image/svg+xml"></object>
+                
+                <object data={WaveOne} type="image/svg+xml" className="wave"></object>
+                <object data={WaveTwo} type="image/svg+xml" className="wave"></object>
+                <object data={WaveThree} type="image/svg+xml" className="wave"></object>
+
+                <WaveTest width={width} height={height} />
 
             {/* </div> */}
         </header>
