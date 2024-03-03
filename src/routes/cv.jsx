5baf4939f85609af59ffaf87d54bd90d9ffba5c1
Monica Björk <m.bjork95@gmail.com>
2024-03-03 21:06:08 +0100

    styled projects and cv data

--- src/routes/cv.jsx
+++ src/routes/cv.jsx
@@ -1,18 +1,19 @@
 import cvData from "../data/cv.json";
 import parse from "html-react-parser";
+import "./cv.css";
 
 export default function CV() {
 
     const cvDataDisplay = cvData.map((category, x) => {
-        for(let key in category) {
-            const items = category[key].map((item, y) => {
+        for(let categoryKey in category) {
+            const items = category[categoryKey].map((item, y) => {
                 console.log(item);
                 
                 const cvKey = "cv-" + x + "-" + y;
                 
                 return (
-                    <section key={cvKey}>
-                        <h3>{item.title}</h3>
+                    <section className="cvItem" key={cvKey}>
+                        <p>{item.title}</p>
                         <p>{item.time.start} - {item.time.end}</p>
                         <address>{item.location}</address>
                         <p>{parse(item.description)}</p>
@@ -21,18 +22,16 @@ export default function CV() {
             });
 
             return (
-                <>
-                    <h2>{key}</h2>
+                <section className="cvCategory">
+                    <h2>{categoryKey}</h2>
                     {items}
-                </>
+                </section>
             );
         }
     });
 
     return (
         <article>
-            <h1>CV</h1>
-            <i>Ett urval</i>
             {cvDataDisplay}
         </article>
     );
--- src/routes/projects.jsx
+++ src/routes/projects.jsx
@@ -23,8 +23,7 @@ export default function Projects() {
 
     return (
         <>
-        <h1>Projekt</h1>
-        {projectsDataDisplay}
+            {projectsDataDisplay}
         </>
     );
 }
