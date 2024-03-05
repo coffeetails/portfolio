445aee54a3141eae69ddd345934d4e2a33332b77
Monica Björk <m.bjork95@gmail.com>
2024-03-05 23:32:56 +0100

    added simple contact info

--- src/routes/connect.jsx
+++ src/routes/connect.jsx
@@ -1,7 +1,14 @@
+import "./connect.css";
 
 export default function Connect() {
 
     return (
-        <p>Connect</p>
+        <article>
+            <h2>Kontakt</h2>
+            <address className="contact">
+                <a href="mailto:m.bjork95@gmail.com">m.bjork95@gmail.com</a><br />
+                <a href="tel:+46709667534">+46 709 66 75 24</a>
+            </address>
+        </article>
     );
 }
