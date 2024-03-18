48c2df958a9ca037fbf03add8ae2afaffe383089
Monica Björk <m.bjork95@gmail.com>
2024-03-18 20:12:28 +0100

    some cleanup and added a dialog

--- src/routes/connect.tsx
+++ src/routes/connect.tsx
@@ -1,18 +1,26 @@
 // import React, { useEffect, useState , useRef } from "react";
-import { SetStateAction, useRef, useState } from "react";
+import { SetStateAction, useRef, useState, useEffect } from "react";
 import emailjs from "@emailjs/browser";
 import ReCAPTCHA from "react-google-recaptcha";
 import "./connect.css";
 
 export default function Connect() {
-    const formRef = useRef<HTMLFormElement | null>(null);
     const captchaRef = useRef<ReCAPTCHA>(null);
+    const dialogRef = useRef<HTMLDialogElement>(null);
     const [userName, setUserName] = useState<string>("");
     const [userEmail, setUserEmail] = useState<string>("");
     const [message, setMessage] = useState<string>("");
+    const [submitDisabled, setSubmitDisabled] = useState<boolean>(false);
+    
+    // Debug
+    // useEffect(() => {
+    //     dialogRef.current!.show();
+    //     dialogRef.current!.close();
+    // });
     
     const submitForm = (event: { preventDefault: () => void; }) => {
         event.preventDefault();
+        setSubmitDisabled(true);
         const captchaToken = captchaRef.current!.getValue();
         emailjs.init({"publicKey": '9liwcYmbKSut2URU9'}); // Public key and user id is the same.
         
@@ -32,14 +40,17 @@ export default function Connect() {
         .then(
             () => {
                 console.log('SUCCESS!');
+                dialogRef.current!.show();
             },
             (error) => {
                 console.log('FAILED...', error);
+                setSubmitDisabled(false);
             },
         );
-
+        
         captchaRef.current!.reset();
     };
+        
 
     const updateUserName = (event: { target: { value: SetStateAction<string>; }; }) => {
         setUserName(event.target.value);
@@ -67,7 +78,7 @@ export default function Connect() {
                 
             <section>
                 <p>Eller skicka något direkt till min mail nedan: </p>
-                <form className="email-form" ref={formRef} onSubmit={submitForm}>
+                <form className="email-form" onSubmit={submitForm}>
                     <label>Namn: </label>
                     <input type="text" name="user_name"  placeholder="John Doe" value={userName} onChange={updateUserName} required  />
                     <label>Email: </label>
@@ -75,8 +86,12 @@ export default function Connect() {
                     <label>Meddelande: </label>
                     <textarea name="message" placeholder="Hej, vilken spännande portfolio du har! Jag tänkte att..." value={message} onChange={updateMessage}  required ></textarea>
                     <ReCAPTCHA sitekey="6LfjvYwpAAAAAEygXYHHuB1orYfnBZPlq-Ix3pjA" ref={captchaRef} theme="dark" className="captchaEmail" />
-                    <input type="submit" value="Skicka" />
+                    <input type="submit" value="Skicka" disabled={submitDisabled} />
                 </form>
+
+                <dialog ref={dialogRef} className="formSuccessDialog">
+                    <p>Tack för ditt meddelande {userName}! Jag svarar så fort jag kan, vilket bör vara inom ett par dagar.</p>
+                </dialog>
             </section>
         </article>
     );
