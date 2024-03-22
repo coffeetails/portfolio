27f930ee9d5ed8d4a87537f141b3523aad374f12
Monica Björk <m.bjork95@gmail.com>
2024-03-22 11:19:52 +0100

    Updates subtitle based on page

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,4 +1,4 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 // <Outlet /> is where the content will show
 import { useLayoutEffect, useState, useRef } from "react";
 
@@ -14,12 +14,34 @@ export default function Root() {
     const [height, setHeight] = useState<number>(0);
     const [width, setWidth] = useState<number>(0);
     const [viewBoxValue, setViewBoxValue] = useState<string>("0 0 0 0");
+    const location = useLocation();
+    let subtitle = "";
 
     useLayoutEffect(() => {
         setHeight(headerElem.current!.offsetHeight)
         setWidth(headerElem.current!.offsetWidth);
         setViewBoxValue("0 0 " + width + " " + height);
     });
+
+    switch (location.pathname) {
+        case "/":
+            subtitle = "Hem";
+            break;
+        case "/cv":
+            subtitle = "CV";
+            break;
+        case "/projects":
+            subtitle = "Projekt";
+            break;
+        case "/connect":
+            subtitle = "Kontakt";
+            break;
+        default:
+            subtitle = "";
+            break;
+    }
+
+
 
     return (
         <>
@@ -29,7 +51,7 @@ export default function Root() {
             <WaveThree width={width} height={height} viewBoxValue={viewBoxValue} />
             <div className="header-text">
                 <h1>Monica Björk</h1>
-                <p>underrubrik</p>
+                <p>{subtitle}</p>
             </div>
             <img src="cv-photo.png" className="cvPhoto" />
             <div className="borderShadow"> </div>
@@ -39,7 +61,7 @@ export default function Root() {
             <nav>
                 <ul>
                     <li><Link to={`/`}>Hem</Link></li>
-                    <li><Link to={`/cv`}>CV</Link></li>
+                    <li><Link to={`/cv`} >CV</Link></li>
                     <li><Link to={`/projects`}>Projekt</Link></li>
                     <li><Link to={`/connect`}>Kontakt</Link></li>
                 </ul>
