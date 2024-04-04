5bf34be962ba3b18e8e4891fde06fd75321cb4b9
Monica Björk <m.bjork95@gmail.com>
2024-04-04 21:24:15 +0200

    adding some backgrounds

--- src/routes/components/techstack.tsx
+++ src/routes/components/techstack.tsx
@@ -16,17 +16,54 @@ export default function TechStack() {
             return;
         }
 
-        context.fillStyle = 'black';
-        context.fillRect(0, 0, canvasWidth, canvasHeight);
+        // context.fillStyle = 'black';
+        // context.fillRect(0, 0, canvasWidth, canvasHeight);
 
-        context.fillStyle = "#fafafa"
+        /* Rectangle 77 */
+
+// position: absolute;
+// left: 100.64%;
+// right: -101.27%;
+// top: 30.62%;
+// bottom: 6.4%;
+
+// /* gradient 2 */
+// background: linear-gradient(180deg, #38761D 4.09%, #0C343D 80.34%);
+// transform: matrix(-1, 0, 0, 1, 0, 0);
+
+        context.fillStyle = "#fafafa";
         context.font = '2rem "Nunito"';
         context.fillText("Techstack", 10, 50);
+        
         context.font = '1rem "Nunito"';
         context.fillText("Grundläggande kunskaper", 30, 100);
         context.fillText("Goda kunskaper",          60, 120);
         context.fillText("Mycket goda kunskaper",   90, 140);
+
+
+        const techs: string[] = ["HTML","CSS", "SASS", "Javascript", "Typescript", "Figma", "Node.js", "React", "Git / github", "Docker", "C#" ];
+        let gradient = context.createLinearGradient(0, 130, 0, 365);
+        gradient.addColorStop(0.4, "#38761D");
+        gradient.addColorStop(0.8, "#0C343D");
+        context.fillStyle = gradient;
+
+        techs.map((tech, i) => {
+            console.log(i, tech);
+        });
+
+        context.fillRect(130, 165, canvasWidth, 18);
+        context.fillRect(130, 185, canvasWidth, 18);
+        context.fillRect(130, 205, canvasWidth, 18);
+        context.fillRect(130, 225, canvasWidth, 18);
+        context.fillRect(130, 245, canvasWidth, 18);
+        context.fillRect(130, 265, canvasWidth, 18);
+        context.fillRect(130, 285, canvasWidth, 18);
+        context.fillRect(130, 305, canvasWidth, 18);
+        context.fillRect(130, 325, canvasWidth, 18);
+        context.fillRect(130, 345, canvasWidth, 18);
+        context.fillRect(130, 365, canvasWidth, 18);
         
+        context.fillStyle = "#fafafa";
         context.fillText("HTML",            140, 180);
         context.fillText("CSS",             140, 200);
         context.fillText("SASS",            140, 220);
