fb6fba6c9785ec9d6d3e33a0290110f30b733ddb
Monica Björk <m.bjork95@gmail.com>
2025-01-15 16:02:08 +0100

    added threejs

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,8 +1,10 @@
 import { Outlet, Link, useLocation } from "react-router-dom";
 // <Outlet /> is where the content will show
-import { useLayoutEffect, useState, useRef } from "react";
+import { useLayoutEffect, useState, useRef, useEffect } from "react";
 
 import './root.css';
+
+import * as THREE from 'three';
 
 import WaveOne from './components/waveOne';
 import WaveTwo from './components/waveTwo';
@@ -10,7 +12,9 @@ import WaveThree from './components/waveThree';
 
 
 export default function Root() {
+    const canvas = useRef(null);
     const headerElem = useRef<HTMLElement | null>(null);
+    
     const homeLink = useRef<HTMLAnchorElement | null>(null);
     const cvLink = useRef<HTMLAnchorElement | null>(null);
     const projectsLink = useRef<HTMLAnchorElement | null>(null);
@@ -54,12 +58,88 @@ export default function Root() {
             setWidth(headerElem.current!.offsetWidth);
             setViewBoxValue("0 0 " + width + " " + height);
         }
-
     });
+
+    useEffect(() => {
+        var scene = new THREE.Scene();
+        scene.background = new THREE.Color( 'darkgray' );
+    
+        var camera = new THREE.PerspectiveCamera( 30, window.innerWidth/window.innerHeight );
+        camera.position.set( 0, 0, 10 );
+        camera.lookAt( scene.position );
+    
+        //@ts-ignore
+        const renderer = new THREE.WebGLRenderer({ canvas: canvas.current });        
+
+        // window.addEventListener( "resize", (event) => {
+        //     camera.aspect = window.innerWidth/window.innerHeight;
+        //     camera.updateProjectionMatrix( );
+        //     renderer.setSize( window.innerWidth, window.innerHeight );
+        // });
+    
+        var ambientLight = new THREE.AmbientLight( 'white', 0.5 );
+        scene.add( ambientLight );
+    
+        var light = new THREE.DirectionalLight( 'white', 0.5 );
+        light.position.set( 1, 1, 1 );
+        scene.add( light );
+    
+        // three points defining the curve
+        var A = new THREE.Mesh(
+            new THREE.SphereGeometry( 0.1 ),
+            new THREE.MeshBasicMaterial( {color:'black'} )
+        ),
+        B = A.clone(),
+        C = A.clone();
+        scene.add( A, B, C );
+    
+        // a Bezier curve based on three points
+        var curve = new THREE.QuadraticBezierCurve3(
+            A.position,
+            B.position,
+            C.position
+        );
+    
+        // a plane that adopts the silhouette of the curve
+        var plane = new THREE.Mesh(
+            new THREE.PlaneGeometry( 1, 1, 20, 1 ),
+            new THREE.MeshBasicMaterial( {color: 'tomato'} )
+        );
+        scene.add( plane );
+    
+        // vertices of the plane
+        var pos = plane.geometry.getAttribute( 'position' ),
+            n = pos.count/2,
+            pnt = new THREE.Vector3(),
+            tan = new THREE.Vector3();
+    
+    
+        function animationLoop( t: number ) {
+            // move the three points up and down
+            A.position.set( -2, Math.sin( t/700 ), 0 );
+            B.position.set(  0, Math.cos( t/800 ), 0 );
+            C.position.set(  2, Math.sin( t/900 ), 0 );
+        
+            // update the plane
+            for( var i=0; i<n; i++ ) {
+                curve.getPoint( i/(n-1), pnt );
+                curve.getTangent( i/(n-1), tan );
+            
+                pos.setXY( i, pnt.x-tan.y, pnt.y+tan.x );		
+                pos.setXY( i+n, pnt.x+tan.y, pnt.y-tan.x );		
+            }
+            pos.needsUpdate = true;
+            
+            renderer.render( scene, camera );
+        }
+    
+        renderer.setAnimationLoop( animationLoop );
+    
+    }, []);
     
     [homeLink, cvLink, projectsLink, connectLink, photographyLink].forEach(elem => {
         if(elem.current) {
-            console.log(elem.current.pathname, location.pathname, elem.current.pathname == location.pathname);
+            // console.log(elem.current.pathname, location.pathname, elem.current.pathname == location.pathname);
             
             if(elem.current.pathname == location.pathname) {
                 elem.current.className = "currentPage";
@@ -70,12 +150,16 @@ export default function Root() {
         }
     });
 
+
     return (
         <>
         <header ref={headerElem}>
-            <WaveOne width={width} height={height} viewBoxValue={viewBoxValue} />
+            <canvas width={window.innerWidth} height={window.innerHeight} ref={canvas} style={{border: "1px solid red"}} />
+            {/* <canvas width={width} height={height} ref={canvas} style={{border: "1px solid red"}} /> */}
+            {/* <WaveOne width={width} height={height} viewBoxValue={viewBoxValue} />
             <WaveTwo width={width} height={height} viewBoxValue={viewBoxValue} />
-            <WaveThree width={width} height={height} viewBoxValue={viewBoxValue} />
+            <WaveThree width={width} height={height} viewBoxValue={viewBoxValue} /> */}
+
             <div className="header-text">
                 <h1>Monica Björk</h1>
                 {/* <p>{subtitle}</p> */}
