52d9d2f9ea23ab8b5c81ceac4124b058b7c4c2b7
Monica Björk <m.bjork95@gmail.com>
2025-01-25 23:25:59 +0100

    the waves are wavey

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,8 +1,5 @@
 import { Outlet, Link, useLocation } from "react-router-dom";
-// <Outlet /> is where the content will show
 import { useLayoutEffect, useState, useRef, useEffect } from "react";
-// import { BufferGeometryUtils } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
-import { RoundedBoxGeometry } from 'three/examples/jsm/geometries/RoundedBoxGeometry.js';
 
 
 import './root.css';
@@ -26,100 +23,58 @@ export default function Root() {
 
     const [height, setHeight] = useState<number>(0);
     const [width, setWidth] = useState<number>(0);
-    // const [viewBoxValue, setViewBoxValue] = useState<string>("0 0 0 0");
+    const [fps, setFps] = useState<number>(0);
+
     const location = useLocation();
-    // let subtitle = "";
-
+    let frames = 0, prevTime = performance.now();
     
-    // function useWindowSize() {
-    //     const [size, setSize] = useState([0, 0]);
-    //     useLayoutEffect(() => {
-    //     function updateSize() {
-    //         setSize([window.innerWidth, window.innerHeight]);
-    //     }
-    //     window.addEventListener('resize', updateSize);
-    //     updateSize();
-    //     return () => window.removeEventListener('resize', updateSize);
-    //     }, []);
-    //     return size;
-    // } // https://stackoverflow.com/questions/19014250/rerender-view-on-browser-resize-with-react
-    // let windowSize = useWindowSize();
-    // let canvasElem; 
-    const reducedHeight = 0.8;
-    const mediaQueryBreakpoint = 800;
-
+    // Set height and with on canvas
     useLayoutEffect(() => {
-        // console.log("header", headerElem.current!.offsetWidth + " x " + headerElem.current!.offsetHeight);
-        // console.log("window", windowSize);
-        
-        if(headerElem.current!.offsetWidth < mediaQueryBreakpoint) {
-            setHeight(headerElem.current!.offsetHeight*reducedHeight);
-            setWidth(headerElem.current!.offsetWidth);
-        } else {
-            setHeight(headerElem.current!.offsetHeight);
-            setWidth(headerElem.current!.offsetWidth);
-        }
+        setHeight(headerElem.current!.offsetHeight);
+        setWidth(headerElem.current!.offsetWidth);
     }, [headerElem.current]);
+
 
     useEffect(() => {
         if (height > 0 && width > 0) {
         var scene = new THREE.Scene();
-        scene.background = new THREE.Color( 'darkgray' );
     
         var camera = new THREE.PerspectiveCamera( 30, width/height );
-        // var camera = new THREE.PerspectiveCamera( 30, window.innerWidth/(window.innerHeight-400) );
         camera.position.set( 0, 0, 10 );
-        // camera.lookAt( scene.position );
-    
+        camera.aspect = width / height;
+        
         //@ts-ignore
-        const renderer = new THREE.WebGLRenderer({ canvas: canvas.current });
+        const renderer = new THREE.WebGLRenderer({ canvas: canvas.current, alpha: true, antialias: true });
 
-        window.addEventListener( "resize", () => { 
+        window.addEventListener( "resize", () => {
+            // Unsure why, but the state refuses to update
+            // inside of useEffect. But fps works fine.
+            // This is a mystery to be solved for a noob
+            // like me. Mañana.
             const newWidth = headerElem.current!.offsetWidth;
             const newHeight = headerElem.current!.offsetHeight;
-            
+
             camera.aspect = newWidth / newHeight;
             camera.updateProjectionMatrix();
             renderer.setSize(newWidth, newHeight);
         });
     
 
-        // var ambientLight = new THREE.AmbientLight( 'white', 0.5 );
-        // scene.add( ambientLight );
+        var ambientLight = new THREE.AmbientLight( 'white', 0.5 );
+        scene.add( ambientLight );
     
-        const skyColor = 0xB1E1FF;  // light blue
-        const groundColor = 0xB97A20;  // brownish orange
-        const intensity = 1;
-        const hemisphereLight = new THREE.HemisphereLight(skyColor, groundColor, intensity);
-        scene.add(hemisphereLight);
-
-        var directionalLight = new THREE.DirectionalLight( 'white', 10 );
-        directionalLight.position.set( -13, 10, -10 );
-        scene.add( directionalLight );
-
 
         // Plane Geometry
-        const widthPlane = 5, heightPlane = 1, segments = 50;
-        const geometry = new THREE.PlaneGeometry(widthPlane, heightPlane, segments, segments);
-        geometry.rotateY(Math.PI / 2); 
+        const widthPlane = 5, heightPlane = 1, segments = 100;
+        const geometryOne  = new THREE.PlaneGeometry(widthPlane, heightPlane, segments, segments/6);
+        const geometryTwo  = new THREE.PlaneGeometry(widthPlane+4, heightPlane, segments*1.5, segments/6);
+        const geometryThree = new THREE.PlaneGeometry(widthPlane+8, heightPlane, segments*2, segments/6);
+        geometryOne.rotateY(Math.PI / 2); 
+        geometryTwo.rotateY(Math.PI / 2); 
+        geometryThree.rotateY(Math.PI / 2);   
 
-        // // Create a rounded corner effect
-        // const radius = 1;
-        // const position = geometry.attributes.position;
-        // for (let i = 0; i < position.count; i++) {
-        //     let x = position.getX(i);
-        //     let y = position.getY(i);
-            
-        //     // Clamp corners to create a rounded effect
-        //     let d = Math.sqrt((x * x) + (y * y));
-        //     if (d < heightPlane / 2 - radius && d < widthPlane / 2 - radius) {
-        //         let scale = (heightPlane / 2 - radius) / d;
-        //         position.setXY(i, x * scale, y * scale);
-        //     }
-        // }
 
-        // Material
-        const material = new THREE.ShaderMaterial({
+        const materialOne = new THREE.ShaderMaterial({
             vertexShader: `
                 uniform float time;
                 varying vec2 vUv;
@@ -132,7 +87,62 @@ export default function Root() {
                     pos.x = pos.z;
                     pos.z = -temp;
 
-                    float wave = sin(pos.x * 2.0 + time * 1.0) * 0.1; 
+                    float wave = sin(pos.x * 2.0 + time * 0.03) * 0.1; 
+                    pos.y += wave;
+                    gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+                }
+            `,
+            fragmentShader: `
+                varying vec2 vUv;
+                uniform vec3 colors[5];
+
+                void main() {
+                    // Calculate the gradient based on UV coordinates
+                    float steps[4] = float[4](0.25, 0.25, 0.25, 0.25); // 4 steps between 5 colors
+                    int length = 4;
+                    vec3 color = vec3(0.0);
+                    
+                    for (int i = 0; i < length; i++) {
+                        if( vUv.y < (steps[i] * float(i + 1)) ) {
+                            float t = (vUv.y - steps[i]) / steps[i];
+                            color = mix(colors[i], colors[i + 1], t);
+                            break;
+                        }
+                    }
+
+                    gl_FragColor = vec4(color, 1.0);
+                }
+            `,
+            uniforms: {
+                time: { value: 0 },
+                colors: { 
+                    value: [
+                        new THREE.Color(0.2, 0.0, 0.2), // dark green
+                        new THREE.Color(0.0, 0.0, 0.2), // dark green
+                        new THREE.Color(0.2, 0.0, 0.0), // dark green
+                        new THREE.Color(0.0, 0.2, 0.0), // dark green
+                        new THREE.Color(0.1, 0.3, 0.1), // top, bluegreenish
+                    ]   
+                }
+            },
+            side: THREE.DoubleSide,
+            wireframe: false,
+        });
+
+        const materialTwo = new THREE.ShaderMaterial({
+            vertexShader: `
+                uniform float time;
+                varying vec2 vUv;
+                void main() {
+                    vUv = uv;
+                    vec3 pos = position;
+
+                    // Rotate 90 degrees around the Y-axis
+                    float temp = pos.x;
+                    pos.x = pos.z;
+                    pos.z = -temp;
+
+                    float wave = cos(pos.x * 2.0 + time * 0.08) * 0.15; 
                     pos.y += wave;
                     gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
                 }
@@ -140,7 +150,7 @@ export default function Root() {
             fragmentShader: `
                 varying vec2 vUv;
                 void main() {
-                    gl_FragColor = vec4(0.5, 0.5 + 0.5 * cos(vUv.y * 6.5), 0, 1); // Gradient color
+                    gl_FragColor = vec4(0.1, 0.5 + 0.5 * cos(vUv.y * 4.7), 0.1, 0.5); // Gradient color
                 }
             `,
             uniforms: {
@@ -150,16 +160,68 @@ export default function Root() {
             wireframe: false,
         });
 
-        const wavePlane = new THREE.Mesh(geometry, material);
-        wavePlane.position.set(0, -0.5, 5);
-        // wavePlane.rotateX(0.75);
-        scene.add(wavePlane);
+        const materialThree = new THREE.ShaderMaterial({
+            vertexShader: `
+                uniform float time;
+                varying vec2 vUv;
+                void main() {
+                    vUv = uv;
+                    vec3 pos = position;
+
+                    // Rotate 90 degrees around the Y-axis
+                    float temp = pos.x;
+                    pos.x = pos.z;
+                    pos.z = -temp;
+
+                    float wave = sin(pos.x * 2.0 + time * 0.15) * 0.2; 
+                    pos.y += wave;
+                    gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+                }
+            `,
+            fragmentShader: `
+                varying vec2 vUv;
+                void main() {
+                    gl_FragColor = vec4(0.1, 0.5 + 0.5 * cos(vUv.y * 4.7), 0.1, 0.5); // Gradient color
+                }
+            `,
+            uniforms: {
+                time: { value: 0 }
+            },
+            side: THREE.DoubleSide,
+            wireframe: false,
+        });
+
+        const wavePlaneOne   = new THREE.Mesh(geometryOne, materialOne);
+        const wavePlaneTwo   = new THREE.Mesh(geometryTwo, materialTwo);
+        const wavePlaneThree = new THREE.Mesh(geometryThree, materialThree);
+        // wavePlaneOne.position.set(0, 0, 6);
+        wavePlaneOne.position.set(0, -0.6, 9);
+        wavePlaneTwo.position.set(0, -0.5, 8.5);
+        wavePlaneThree.position.set(0, -0.35, 8);
+        wavePlaneOne.rotateY(3);
+        wavePlaneTwo.rotateY(3);
+        wavePlaneThree.rotateY(3);
+        scene.add(wavePlaneOne);
+        scene.add(wavePlaneTwo);
+        scene.add(wavePlaneThree);
         const clock = new THREE.Clock();
 
 
 
         function animationLoop() {
-            material.uniforms.time.value = clock.getElapsedTime();
+            frames ++;
+            const time = performance.now();
+            
+            if ( time >= prevTime + 1000 ) {
+                // console.log( "fps", Math.round( ( frames * 1000 ) / ( time - prevTime ) ) );
+                setFps(Math.round( ( frames * 1000 ) / ( time - prevTime ) ));
+                frames = 0;
+                prevTime = time;
+            }
+
+            materialOne.uniforms.time.value   = clock.getElapsedTime();
+            materialTwo.uniforms.time.value   = clock.getElapsedTime();
+            materialThree.uniforms.time.value = clock.getElapsedTime();
             renderer.render( scene, camera );
         }
     
@@ -167,28 +229,23 @@ export default function Root() {
         }
     }, [height, width, headerElem.current]);
     
+
     [homeLink, cvLink, projectsLink, connectLink, photographyLink].forEach(elem => {
         if(elem.current) {
-            // console.log(elem.current.pathname, location.pathname, elem.current.pathname == location.pathname);
-            
             if(elem.current.pathname == location.pathname) {
                 elem.current.className = "currentPage";
             } else {
                 elem.current.className = "";
             }
-            
         }
     });
+
 
     return (
         <>
         <header ref={headerElem}>
-            {/* <canvas width={window.innerWidth} height={window.innerHeight-350} ref={canvas} /> */}
             <canvas width={width} height={height} ref={canvas} />
-
-            {/* <WaveOne width={width} height={height} viewBoxValue={viewBoxValue} />
-            <WaveTwo width={width} height={height} viewBoxValue={viewBoxValue} />
-            <WaveThree width={width} height={height} viewBoxValue={viewBoxValue} /> */}
+            <p style={{position: "absolute", right: "0.5rem", bottom: "-0.75rem", color: "black", opacity: "0.5", fontSize: "0.75rem"}}>{fps}fps</p>
 
             <div className="header-text">
                 <h1>Monica Björk</h1>
